6a0a21660c713db2491d903fb6c4d777e5b15d9a
nayeon Kim <kimnayeon574@gmail.com>
2019-05-28 15:37:17 +0900

    #1 ListView 컨트롤을 추가 및 각 컨트롤 설정

--- ListView/Form1.cs
+++ ListView/Form1.cs
@@ -11,9 +11,35 @@ namespace ListView
 {
     public partial class Form1 : Form
     {
+        string strName;
+        string strAge;
+        string strWork;
+
         public Form1()
         {
             InitializeComponent();
+        }
+        
+        private bool TextCheck()
+        {
+            if(this.txtName.Text == "")
+            {
+                MessageBox.Show("이름을 입력하세요", "알림", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                this.txtName.Focus();
+                return false;
+            }
+            if(this.txtAge.Text == "")
+            {
+                MessageBox.Show("나이를 입력하세요", "알림", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                this.txtAge.Focus();
+                return false;
+            }
+            if (this.txtWork.Text == "")
+            {
+                MessageBox.Show("직업을 입력하세요", "알림", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                this.txtWork.Focus();
+                return false;
+            }
         }
     }
 }
